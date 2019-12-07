e56d6723cb7cd596734f66872e66bb38cf57b844
lalafish2019 <58617434+lalafish2019@users.noreply.github.com>
2019-12-07 19:03:17 +0800

    black and white color scheme

--- assets/js/game-of-life-v3.1.1.js
+++ assets/js/game-of-life-v3.1.1.js
@@ -62,7 +62,7 @@
 
     // Trail state
     trail : {
-      current: true,
+      current: false,
       schedule : false
     },
 
@@ -125,18 +125,7 @@
       schedule : false,
 
       schemes : [
-      {
-        dead : '#FFFFFF',
-        trail : ['#B5ECA2'],
-        alive : ['#9898FF', '#8585FF', '#7272FF', '#5F5FFF', '#4C4CFF', '#3939FF', '#2626FF', '#1313FF', '#0000FF', '#1313FF', '#2626FF', '#3939FF', '#4C4CFF', '#5F5FFF', '#7272FF', '#8585FF']
-      },
-
-      {
-        dead : '#FFFFFF',
-        trail : ['#EE82EE', '#FF0000', '#FF7F00', '#FFFF00', '#008000 ', '#0000FF', '#4B0082'],
-        alive : ['#FF0000', '#FF7F00', '#FFFF00', '#008000 ', '#0000FF', '#4B0082', '#EE82EE']
-      },
-
+      
       {
         dead : '#FFFFFF',
         trail : ['#9898FF', '#8585FF', '#7272FF', '#5F5FFF', '#4C4CFF', '#3939FF', '#2626FF', '#1313FF', '#0000FF', '#1313FF', '#2626FF', '#3939FF', '#4C4CFF', '#5F5FFF', '#7272FF', '#8585FF'],
